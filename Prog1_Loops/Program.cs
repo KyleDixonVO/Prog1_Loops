26dca535cedff06e46856375bfc4dad1f6d3c4e7
KyleDixonVO <KyleDixon@hotmail.ca>
2021-10-18 12:35:14 -0300

    Before/After using loops

--- Prog1_Loops/Program.cs
+++ Prog1_Loops/Program.cs
@@ -10,6 +10,94 @@ namespace Prog1_Loops
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Before Loops");
+
+            Console.WriteLine(1);
+            Console.WriteLine(2);
+            Console.WriteLine(3);
+            Console.WriteLine(4);
+            Console.WriteLine(5);
+            Console.WriteLine(6);
+            Console.WriteLine(7);
+            Console.WriteLine(8);
+            Console.WriteLine(9);
+            Console.WriteLine(10);
+            Console.WriteLine();
+
+            int count = 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine();
+            //etc.
+            Console.ReadKey(true);
+
+            Console.WriteLine("After Loops");
+            Console.WriteLine();
+
+
+            // "i" stands for index in standard practice
+            // "for loop" components:
+            // 1) declaration/initialization of loop variable
+            // 2) "stop" condition
+            // 3) increment loop variable
+            for (int i = 1; i <= 10; i++)
+            {
+
+                Console.WriteLine(i);
+               
+                //Console.ReadKey(true); 
+
+            }
+
+            Console.ReadKey(true);
         }
     }
 }
